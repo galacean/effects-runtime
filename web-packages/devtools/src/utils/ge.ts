47f67597f8a6e3abbab03104248d590471c2ca1d
wumaolinmaoan <wumaolin1998@outlook.com>
2024-01-11 19:52:52 +0800

    feat: add save button

--- web-packages/devtools/src/utils/ge.ts
+++ web-packages/devtools/src/utils/ge.ts
@@ -1,4 +1,5 @@
-import { Player } from '@galacean/effects';
+import type { Composition, EffectsObjectData, VFXItem, VFXItemContent } from '@galacean/effects';
+import { DataType, Player, TimelineComponent, spec } from '@galacean/effects';
 import json from '../assets/scenes/trail-demo2.scene.json';
 import { Input } from '../gui/input';
 import { InspectorGui } from '../gui/inspector-gui';
@@ -9,16 +10,19 @@ export const treeGui = new TreeGui();
 const inspectorGui = new InspectorGui();
 let input: Input;
 let orbitController: OrbitController;
+let composition: Composition;
+let player: Player;
 
 export async function initGEPlayer (canvas: HTMLCanvasElement) {
-  const player = new Player({ canvas, interactive: true, notifyTouch: true });
-  //@ts-expect-error
-  const comp = await player.loadScene(json);
+  player = new Player({ canvas, interactive: true, notifyTouch: true });
 
-  treeGui.setComposition(comp);
+  //@ts-expect-error
+  composition = await player.loadScene(json);
+
+  treeGui.setComposition(composition);
   input = new Input(canvas);
   input.startup();
-  orbitController = new OrbitController(comp.camera, input);
+  orbitController = new OrbitController(composition.camera, input);
   inputControllerUpdate();
 
   setInterval(() => {
@@ -39,3 +43,194 @@ function inputControllerUpdate () {
   input.refreshStatus();
   requestAnimationFrame(inputControllerUpdate);
 }
+
+async function saveJSONFile (json: any) {
+  // 创建一个包含JSON数据的对象
+  const myData = json;
+
+  // 将JSON对象转换为字符串
+  const jsonStr = JSON.stringify(myData, null, 2);
+
+  try {
+    // 显示文件保存对话框，用户可以选择文件夹并输入文件名
+    //@ts-expect-error
+    const handle = await window.showSaveFilePicker({
+      suggestedName: 'trail-demo.scene.json',
+      types: [
+        {
+          description: 'JSON files',
+          accept: { 'application/json': ['.json'] },
+        },
+      ],
+    });
+
+    // 创建一个FileSystemWritableFileStream来写入选定的文件
+    const writableStream = await handle.createWritable();
+
+    // 写入JSON字符串数据
+    await writableStream.write(jsonStr);
+
+    // 关闭文件流
+    await writableStream.close();
+  } catch (error) {
+    console.error('文件保存失败', error);
+  }
+}
+
+async function loadJSONFile () {
+  //@ts-expect-error
+  const fileHandle: FileSystemFileHandle[] = await window.showOpenFilePicker();
+  const file = await fileHandle[0].getFile();
+  const reader = new FileReader();
+
+  reader.onload = async () => {
+    if (typeof reader.result !== 'string') {
+      return;
+    }
+    const data = JSON.parse(reader.result);
+
+    player.destroyCurrentCompositions();
+    composition = await player.loadScene(data);
+    treeGui.setComposition(composition);
+    orbitController.setup(composition.camera, input);
+  };
+  reader.readAsText(file);
+}
+
+function saveScene (composition: Composition, json: any) {
+  const deserializer = composition.getEngine().deserializer;
+  let serializedDatas: Record<string, EffectsObjectData> = {};
+
+  for (const itemData of json.items) {
+    if (itemData.type === spec.ItemType.sprite || itemData.type === spec.ItemType.particle) {
+      continue;
+    }
+    const item = deserializer.getInstance(itemData.id) as VFXItem<VFXItemContent>;
+
+    item.transform.toData();
+    itemData.transform = item.transform.taggedProperties;
+    itemData.name = item.name;
+    for (const component of item.components) {
+      if (component instanceof TimelineComponent) {
+        continue;
+      }
+      serializedDatas = {
+        ...serializedDatas,
+        ...deserializer.serializeEffectObject(component),
+      };
+    }
+  }
+  let effectsObjectDataMap: Record<string, EffectsObjectData> = {};
+
+  for (const data of json.components) {
+    effectsObjectDataMap[data.id] = data;
+  }
+  for (const data of json.geometries) {
+    effectsObjectDataMap[data.id] = data;
+  }
+  for (const data of json.materials) {
+    effectsObjectDataMap[data.id] = data;
+  }
+  for (const data of json.textures) {
+    effectsObjectDataMap[data.id] = data;
+  }
+
+  effectsObjectDataMap = {
+    ...effectsObjectDataMap,
+    ...serializedDatas,
+  };
+
+  json.components = [];
+  json.geometries = [];
+  json.materials = [];
+  json.textures = [];
+  for (const data of Object.values(effectsObjectDataMap)) {
+    if (!data.id) {
+      continue;
+    }
+    switch (data.dataType) {
+      case DataType.EffectComponent:
+        json.components.push(data);
+
+        break;
+      case DataType.SpriteComponent:
+        json.components.push(data);
+
+        break;
+      case DataType.ParticleSystem:
+        json.components.push(data);
+
+        break;
+      case DataType.Material:
+        json.materials.push(data);
+
+        break;
+      case DataType.Geometry:
+        json.geometries.push(data);
+
+        break;
+      case DataType.Texture:
+        json.textures.push(data);
+
+        break;
+    }
+  }
+}
+
+const body = document.querySelector('body');
+const saveButton = document.createElement('button');
+
+saveButton.style.position = 'fixed';
+saveButton.style.bottom = '20px';
+saveButton.style.right = '20px';
+saveButton.style.padding = '10px 15px';
+saveButton.style.backgroundColor = '#007bff';
+saveButton.style.color = 'white';
+saveButton.style.border = 'none';
+saveButton.style.borderRadius = '5px';
+saveButton.style.cursor = 'pointer';
+
+// 添加悬停效果
+saveButton.addEventListener('mouseover', () => {
+  saveButton.style.backgroundColor = '#1fa404';
+});
+
+// 当鼠标移出时恢复原来的背景颜色
+saveButton.addEventListener('mouseout', () => {
+  saveButton.style.backgroundColor = '#007bff';
+});
+
+body?.appendChild(saveButton);
+saveButton.textContent = '保存场景json';
+saveButton.onclick = async () => {
+  saveScene(composition, json);
+  await saveJSONFile(json);
+};
+
+const loadButton = document.createElement('button');
+
+loadButton.style.position = 'fixed';
+loadButton.style.bottom = '20px';
+loadButton.style.right = '160px';
+loadButton.style.padding = '10px 15px';
+loadButton.style.backgroundColor = '#007bff';
+loadButton.style.color = 'white';
+loadButton.style.border = 'none';
+loadButton.style.borderRadius = '5px';
+loadButton.style.cursor = 'pointer';
+
+// 添加悬停效果
+loadButton.addEventListener('mouseover', () => {
+  loadButton.style.backgroundColor = '#1fa404';
+});
+
+// 当鼠标移出时恢复原来的背景颜色
+loadButton.addEventListener('mouseout', () => {
+  loadButton.style.backgroundColor = '#007bff';
+});
+
+body?.appendChild(loadButton);
+loadButton.textContent = '加载场景json';
+loadButton.onclick = async () => {
+  await loadJSONFile();
+};
