eeb5cc8a69758c50652b849ae2ecac276f26753c
YunYouJun <me@yunyoujun.cn>
2024-01-22 19:37:32 +0800

    feat(devtools): update new gui & add inspector

---
+++ web-packages/devtools/src/components/PanelInspector.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const tabList = ref([
+  { title: 'Inspector', key: 'inspector', icon: 'i-ri-information-fill' },
+])
+</script>
+
+<template>
+  <AGUIPanel h="full" w="full">
+    <AGUITabs :list="tabList">
+      <AGUITabPanel>
+        <InspectorView />
+      </AGUITabPanel>
+    </AGUITabs>
+  </AGUIPanel>
+</template>
