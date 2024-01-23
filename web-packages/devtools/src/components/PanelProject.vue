7443d27e1315f332674e0f5a7c8f40aec136d59a
YunYouJun <me@yunyoujun.cn>
2024-01-23 20:17:13 +0800

    feat: add custom icon

--- web-packages/devtools/src/components/PanelProject.vue
+++ web-packages/devtools/src/components/PanelProject.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-import { FSDirItem } from '@advjs/gui';
+import { FSDirItem, FSItem } from '@advjs/gui';
 import { ref, watch } from 'vue';
 import { curDir, onFileDblClick, onFileDrop, onRootFolderSelect } from "../gui/project-gui";
+import { compileScript } from 'vue/compiler-sfc';
 
 const tabList = ref([
   { title: 'Project', key: 'project', icon: 'i-ri-folder-line' },
@@ -18,17 +19,24 @@ watch(rootDir, async () => {
 // }
 // a.onFileDblClick = ()=>{
 // }
+
+function onFSItemChange(item: FSItem) {
+  if (item.name === 'donut.blend') {
+    item.icon = 'i-ri-alipay-line'
+  }
+}
 </script>
 
 <template>
   <AGUIPanel w="full" h="full">
     <AGUITabs :list="tabList">
       <AGUITabPanel>
-        <AGUIAssetsExplorer 
+        <AGUIAssetsExplorer
           v-model:cur-dir="curDir"
           v-model:root-dir="rootDir"
           :onFileDrop="onFileDrop"
-          :onFileDblClick="onFileDblClick" 
+          :onFileDblClick="onFileDblClick"
+          :onFSItemChange="onFSItemChange"
         />
         <slot name="project" />
       </AGUITabPanel>
