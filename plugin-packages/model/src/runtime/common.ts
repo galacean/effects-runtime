79cfb5d01fd63e8ae1518011289113926d017467
liuxi150 <liuxi150@hotmail.com>
2024-02-29 17:30:10 +0800

    docs: update

--- plugin-packages/model/src/runtime/common.ts
+++ plugin-packages/model/src/runtime/common.ts
@@ -11,6 +11,9 @@ import type { BaseTransform } from '../index';
 
 type Euler = math.Euler;
 
+/**
+ * 3D 场景中的对象类型
+ */
 export enum PObjectType {
   none = 0,
   mesh,
@@ -27,6 +30,9 @@ export enum PObjectType {
   reference,
 }
 
+/**
+ * 灯光类型
+ */
 export enum PLightType {
   directional = 0,
   point,
@@ -34,6 +40,9 @@ export enum PLightType {
   ambient,
 }
 
+/**
+ * 纹理类型
+ */
 export enum PTextureType {
   none = 0,
   t2d,
@@ -41,6 +50,9 @@ export enum PTextureType {
   cube,
 }
 
+/**
+ * 材质类型
+ */
 export enum PMaterialType {
   none = 0,
   unlit,
@@ -54,6 +66,9 @@ export enum PMaterialType {
   skyboxFilter,
 }
 
+/**
+ * 混合模式
+ */
 export enum PBlendMode {
   opaque = 0,
   masked,
@@ -61,12 +76,18 @@ export enum PBlendMode {
   additive,
 }
 
+/**
+ * 面片朝向模式
+ */
 export enum PFaceSideMode {
   both = 0,
   front,
   back,
 }
 
+/**
+ * 阴影类型
+ */
 export enum PShadowType {
   none = 0,
   standard,
@@ -74,17 +95,30 @@ export enum PShadowType {
   expVariance,
 }
 
+/**
+ * 3D 变换类
+ */
 export class PTransform {
   private translation = new Vector3(0, 0, 0);
   private rotation = new Quaternion(0, 0, 0, 1);
   private scale = new Vector3(1, 1, 1);
 
+  /**
+   * 从矩阵设置数据
+   * @param matrix 4阶矩阵
+   * @returns
+   */
   fromMatrix4 (matrix: Matrix4) {
     this.setMatrix(matrix);
 
     return this;
   }
 
+  /**
+   * 从 GE 变换设置数据
+   * @param trans GE 变换对象或数据
+   * @returns
+   */
   fromEffectsTransform (trans: EffectsTransform | BaseTransform) {
     if (trans instanceof EffectsTransform) {
       this.setMatrix(trans.getWorldMatrix());
@@ -102,6 +136,11 @@ export class PTransform {
     return this;
   }
 
+  /**
+   * 转成 GE 变换对象
+   * @param transform GE 变换对象
+   * @returns
+   */
   toEffectsTransform (transform: EffectsTransform) {
     const mat = this.getMatrix();
 
@@ -110,6 +149,11 @@ export class PTransform {
     return transform;
   }
 
+  /**
+   * 从 GE 变换数据设置
+   * @param trans 变换数据
+   * @returns
+   */
   fromBaseTransform (trans: BaseTransform) {
     if (trans.position) {
       this.setTranslation(trans.position);
@@ -132,10 +176,18 @@ export class PTransform {
     return this;
   }
 
+  /**
+   * 获取平移
+   * @returns
+   */
   getTranslation (): Vector3 {
     return this.translation;
   }
 
+  /**
+   * 设置平移
+   * @param val 平移
+   */
   setTranslation (val: Vector3 | spec.vec3) {
     if (val instanceof Vector3) {
       this.translation.set(val.x, val.y, val.z);
@@ -144,10 +196,18 @@ export class PTransform {
     }
   }
 
+  /**
+   * 获取位置
+   * @returns
+   */
   getPosition (): Vector3 {
     return this.translation;
   }
 
+  /**
+   * 设置位置
+   * @param val 位置
+   */
   setPosition (val: Vector3 | spec.vec3) {
     if (val instanceof Vector3) {
       this.translation.set(val.x, val.y, val.z);
@@ -156,10 +216,18 @@ export class PTransform {
     }
   }
 
+  /**
+   * 获取旋转
+   * @returns
+   */
   getRotation (): Quaternion {
     return this.rotation;
   }
 
+  /**
+   * 设置旋转
+   * @param val 旋转，可能是四元数或欧拉角
+   */
   setRotation (val: Quaternion | Euler | Vector3 | spec.vec4 | spec.vec3) {
     if (val instanceof Quaternion) {
       this.rotation.set(val.x, val.y, val.z, val.w);
@@ -174,10 +242,18 @@ export class PTransform {
     }
   }
 
+  /**
+   * 获取缩放
+   * @returns
+   */
   getScale (): Vector3 {
     return this.scale;
   }
 
+  /**
+   * 设置缩放
+   * @param val 缩放
+   */
   setScale (val: Vector3 | spec.vec3) {
     if (val instanceof Vector3) {
       this.scale.set(val.x, val.y, val.z);
@@ -186,10 +262,18 @@ export class PTransform {
     }
   }
 
+  /**
+   * 获取矩阵
+   * @returns
+   */
   getMatrix (): Matrix4 {
     return new Matrix4().compose(this.getTranslation(), this.getRotation(), this.getScale());
   }
 
+  /**
+   * 设置矩阵
+   * @param mat 4阶矩阵
+   */
   setMatrix (mat: Matrix4 | spec.mat4) {
     if (mat instanceof Matrix4) {
       const res = mat.getTransform();
@@ -207,10 +291,25 @@ export class PTransform {
   }
 }
 
+/**
+ * 坐标系类
+ */
 export class PCoordinate {
+  /**
+   * 原点
+   */
   origin: Vector3;
+  /**
+   * X 轴
+   */
   xAxis: Vector3;
+  /**
+   * Y 轴
+   */
   yAxis: Vector3;
+  /**
+   * Z 轴
+   */
   zAxis: Vector3;
 
   constructor () {
@@ -220,6 +319,12 @@ export class PCoordinate {
     this.zAxis = new Vector3(0, 0, 1);
   }
 
+  /**
+   * 从 PTransform 创建坐标系
+   * @param trans 变换
+   * @param invert 是否旋转取反
+   * @returns 坐标系对象
+   */
   fromPTransform (trans: PTransform, invert = false) {
     this.origin.copyFrom(trans.getPosition());
     const rotationMatrix = trans.getRotation().toMatrix4(new Matrix4());
@@ -232,6 +337,10 @@ export class PCoordinate {
     return this;
   }
 
+  /**
+   * 从旋转矩阵创建坐标系
+   * @param matrix 矩阵
+   */
   fromRotationMatrix (matrix: Matrix4) {
     const me = matrix.elements;
 
@@ -241,11 +350,29 @@ export class PCoordinate {
   }
 }
 
+/**
+ * 全局状态
+ */
 export class PGlobalState {
+  /**
+   * 是否 WebGL2
+   */
   isWebGL2: boolean;
+  /**
+   * 是否共享 Shader
+   */
   shaderShared: boolean;
+  /**
+   * 运行时环境，编辑器模式或设备模式
+   */
   runtimeEnv: string;
+  /**
+   * 兼容模式，glTF 模式或 Tiny3d 模式
+   */
   compatibleMode: string;
+  /**
+   * 是否显示包围盒
+   */
   visBoundingBox: boolean;
   /**
    * 渲染输出结果模式，可以换中间渲染数据
@@ -259,6 +386,10 @@ export class PGlobalState {
   // singleton related code
   private static instance: PGlobalState;
 
+  /**
+   * 获取单例
+   * @returns
+   */
   static getInstance (): PGlobalState {
     // Do you need arguments? Make it a regular static method instead.
     return this.instance || (this.instance = new this());
@@ -274,6 +405,9 @@ export class PGlobalState {
     this.renderMode3DUVGridSize = 1 / 16;
   }
 
+  /**
+   * 重置数据
+   */
   reset () {
     this.isWebGL2 = false;
     this.shaderShared = true;
@@ -291,24 +425,32 @@ export class PGlobalState {
     return this.renderMode3D !== spec.RenderMode3D.none;
   }
 
+  /**
+   * 是否编辑器模式
+   */
   get isEditorEnv () {
     return this.runtimeEnv === PLAYER_OPTIONS_ENV_EDITOR;
   }
 
+  /**
+   * 是否设备模式
+   */
   get isDeviceEnv () {
     return !this.isEditorEnv;
   }
 
+  /**
+   * 是否 Tiny3d 模式
+   */
   get isTiny3dMode () {
     return this.compatibleMode === 'tiny3d';
   }
 
-  get isOasisMode () {
-    return this.compatibleMode === 'oasis';
-  }
-
+  /**
+   * 是否 glTF 模式
+   */
   get isGLTFMode () {
-    return !this.isTiny3dMode && !this.isOasisMode;
+    return !this.isTiny3dMode;
   }
 }
 
