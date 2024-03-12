3abc2a5070ad8a60616a8f6535b875c0f0af96b8
liuxi150 <liuxi150@hotmail.com>
2024-03-12 14:16:23 +0800

    chore: update demo

--- plugin-packages/alipay-downgrade/demo/src/status.ts
+++ plugin-packages/alipay-downgrade/demo/src/status.ts
@@ -1,4 +1,4 @@
-import { getDowngradeResult } from '@galacean/effects-plugin-alipay-downgrade';
+import { checkDowngradeResult, getDowngradeResult } from '@galacean/effects-plugin-alipay-downgrade';
 
 (async () => {
   let bizId = 'test';
@@ -9,9 +9,14 @@ import { getDowngradeResult } from '@galacean/effects-plugin-alipay-downgrade';
     console.info('Input bizId:', bizId);
   }
 
-  const result = await getDowngradeResult(bizId);
-  const label = document.createElement('label');
+  const downgradeResult = await getDowngradeResult(bizId);
+  const downgradeDecision = checkDowngradeResult(downgradeResult);
+  const resultLabel = document.createElement('label');
 
-  label.innerHTML = `<div><pre>${JSON.stringify(result, undefined, 2)}</pre></div>`;
-  document.body.append(label);
+  resultLabel.innerHTML = `<div><pre>Result:${JSON.stringify(downgradeResult, undefined, 2)}</pre></div>`;
+  const decisionLabel = document.createElement('label');
+
+  decisionLabel.innerHTML = `<div><pre>Decision:${JSON.stringify(downgradeDecision, undefined, 2)}</pre></div>`;
+  document.body.append(resultLabel);
+  document.body.append(decisionLabel);
 })();
