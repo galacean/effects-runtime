4dc629f3af6a6fec9237f91c35631ed1b018fcf8
yiiqii <yfj5tzl2005@sina.com>
2024-04-02 19:00:01 +0800

    chore: 优化加载本地文件 demo

--- web-packages/demo/src/local-file.ts
+++ web-packages/demo/src/local-file.ts
@@ -5,32 +5,10 @@ const container = document.getElementById('J-container');
 
 (async () => {
   try {
-    const player = createPlayer();
+    const player = new Player({ container });
 
-    const comp = await player.loadScene('./assets/find-flower/flower.json');
+    await player.loadScene('./assets/find-flower/flower.json');
   } catch (e) {
     console.error('biz', e);
   }
 })();
-
-function createPlayer () {
-  const player = new Player({
-    container,
-    interactive: true,
-    onPlayableUpdate: ({ player, playing }) => {
-    },
-    // renderFramework: 'webgl',
-    env: 'editor',
-    notifyTouch: true,
-    onPausedByItem: data => {
-      console.info('onPausedByItem', data);
-    },
-    onItemClicked: data => {
-      console.info(`item ${data.name} has been clicked`);
-    },
-    // reportGPUTime: console.debug,
-  });
-
-  return player;
-}
-
