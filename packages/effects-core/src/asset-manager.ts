a99fba27cc4d72d71f436f2cb79801e530f6ed2c
wumaolinmaoan <wumaolin1998@outlook.com>
2024-04-15 15:44:38 +0800

    perf: delete unused function

--- packages/effects-core/src/asset-manager.ts
+++ packages/effects-core/src/asset-manager.ts
@@ -15,7 +15,6 @@ import { deserializeMipmapTexture, TextureSourceType, getKTXTextureOptions, Text
 import type { Renderer } from './render';
 import { COMPRESSED_TEXTURE } from './render';
 import { combineImageTemplate, getBackgroundImage } from './template-image';
-import { version3Migration } from './asset-migrations';
 
 /**
  * 场景加载参数
