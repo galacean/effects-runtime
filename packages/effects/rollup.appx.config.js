85e1f8e9f096038c831e4206660f7f603f239f13
RGCHN <dacong9610@163.com>
2024-05-09 11:16:03 +0800

    feat: 完善小程序打包配置

--- packages/effects/rollup.appx.config.js
+++ packages/effects/rollup.appx.config.js
@@ -2,30 +2,45 @@
  * 小程序产物编译配置
  */
 import inject from '@rollup/plugin-inject';
-const module = '@galacean/appx-adapter';
+
+const module = "@galacean/appx-adapter";
 const commonAdapterList = [
-  'window',
-  'navigator',
-  'HTMLElement',
-  'HTMLImageElement',
-  'HTMLCanvasElement',
-  'HTMLVideoElement',
-  'document',
-  'WebGLRenderingContext',
-  'Image',
-  'URL',
-  'location',
-  'XMLHttpRequest',
-  'Blob',
-  'performance',
-  'requestAnimationFrame',
-  'cancelAnimationFrame',
+  "btoa",
+  "URL",
+  "Blob",
+  "window",
+  "atob",
+  "devicePixelRatio",
+  "document",
+  "Element",
+  "Event",
+  "EventTarget",
+  "HTMLCanvasElement",
+  "HTMLElement",
+  "HTMLMediaElement",
+  "HTMLVideoElement",
+  "Image",
+  "navigator",
+  "Node",
+  "requestAnimationFrame",
+  "cancelAnimationFrame",
+  "screen",
+  "XMLHttpRequest",
+  "performance",
+  "WebGLRenderingContext",
+  "WebGL2RenderingContext",
+  "ImageData",
+  "location",
+  "OffscreenCanvas",
+  "URLSearchParams"
 ];
 const adapterList = {
-  weapp: [...commonAdapterList],
+  // weapp: [...commonAdapterList],
+  alipay: [...commonAdapterList],
 }
 export default [
   'weapp',
+  'alipay'
 ].map(platform => {
   const adapterVars = {};
   adapterList[platform].forEach(name => {
