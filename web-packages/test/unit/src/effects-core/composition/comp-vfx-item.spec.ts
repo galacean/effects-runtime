cb261fd69fe473d7bd97de5b594a1069b65b5b16
Sruim <934274351@qq.com>
2024-05-16 19:23:35 +0800

    feat: 增加 Composition/VFXItem setPositionByPixel API (#357)
    * feat: 从前端开发角度设置 setPositonByPiexl api
    
    * perf: 优化注释
    
    * perf: 优化注释
    
    * perf: 优化测试代码

--- web-packages/test/unit/src/effects-core/composition/comp-vfx-item.spec.ts
+++ web-packages/test/unit/src/effects-core/composition/comp-vfx-item.spec.ts
@@ -749,6 +749,24 @@ describe('comp-vfxItem', () => {
     expect(pos.toArray()[1]).to.closeTo(0, 0.1);
   });
 
+  it('setPosition by pixel', async () => {
+    // FIXME: 老版本数据，需要更新
+    const comp = await player.loadScene('https://mdn.alipayobjects.com/mars/afts/file/A*dFfLQp7L0TsAAAAAAAAAAAAADlB4AQ', {
+      autoplay: false,
+    });
+
+    player.gotoAndStop(1);
+
+    const sprite = comp.getItemByName('sprite_1');
+    const pos = sprite.transform.position;
+
+    const { width, height } = player.canvas.getBoundingClientRect();
+
+    // 正中心
+    sprite?.setPositionByPixel(width / 2, height / 2);
+    expect(pos.toArray()[0]).to.closeTo(0, 0.1);
+    expect(pos.toArray()[1]).to.closeTo(0, 0.1);
+  });
   // 预合成设置缩放
   it('set scale in compVFXItem', async () => {
     const comp = await player.loadScene('https://mdn.alipayobjects.com/mars/afts/file/A*bkLLToY54tgAAAAAAAAAAAAADlB4AQ');
