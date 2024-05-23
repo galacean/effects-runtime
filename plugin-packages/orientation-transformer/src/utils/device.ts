b066a02ddd9140d7c09ea2f1f51942d4d8c8ae84
RGCHN <dacong9610@163.com>
2024-05-23 14:35:49 +0800

    fix: 修改小程序判断方法

--- plugin-packages/orientation-transformer/src/utils/device.ts
+++ plugin-packages/orientation-transformer/src/utils/device.ts
@@ -5,6 +5,16 @@ export function isIOS () {
   return ver ? parseInt(ver[2], 10) : 0;
 }
 
-export function isMiniProgram () {
-  return 'miniProgram' in navigator;
+export function isMiniProgram (): boolean {
+  return isAlipayMiniApp() || isWechatMiniApp();
+}
+
+export function isAlipayMiniApp (): boolean {
+  //@ts-expect-error
+  return typeof my !== 'undefined' && my?.renderTarget === 'web';
+}
+
+export function isWechatMiniApp (): boolean {
+  //@ts-expect-error
+  return window.__wxjs_environment === 'miniprogram';
 }
