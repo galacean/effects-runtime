9602fba1898e48829e168eb2564c9690e972cae6
wumaolinmaoan <wumaolinmaoan@outlook.com>
2024-06-07 15:02:52 +0800

    perf: opt type check

--- packages/effects-core/src/plugins/timeline/playables/sub-composition-clip-playable.ts
+++ packages/effects-core/src/plugins/timeline/playables/sub-composition-clip-playable.ts
@@ -1,13 +1,13 @@
-import type { CompositionComponent } from '../../../comp-vfx-item';
+import { CompositionComponent } from '../../../comp-vfx-item';
 import type { FrameContext } from '../../cal/playable-graph';
 import { Playable } from '../../cal/playable-graph';
 
 export class SubCompositionClipPlayable extends Playable {
 
   override processFrame (context: FrameContext): void {
-    const boundObject = context.output.getUserData() as CompositionComponent;
+    const boundObject = context.output.getUserData();
 
-    if (!boundObject) {
+    if (!(boundObject instanceof CompositionComponent)) {
       return;
     }
 
