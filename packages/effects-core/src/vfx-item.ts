5798f65616ae31d6cd79a87c4ae5741019b7fd75
wumaolinmaoan <wumaolinmaoan@outlook.com>
2024-06-07 17:30:13 +0800

    fix: fix priority setting

--- packages/effects-core/src/vfx-item.ts
+++ packages/effects-core/src/vfx-item.ts
@@ -507,7 +507,6 @@ export class VFXItem extends EffectsObject implements Disposable {
     this.parentId = parentId;
     this.duration = duration;
     this.endBehavior = endBehavior;
-    this.renderOrder = listIndex;
     //@ts-expect-error
     this.oldId = data.oldId;
 
@@ -531,6 +530,7 @@ export class VFXItem extends EffectsObject implements Disposable {
         }
       }
     }
+    this.renderOrder = listIndex;
   }
 
   override toData (): void {
