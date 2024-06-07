f80bc8857f42f712fc12da87c0907022145cda02
yiiqii <yfj5tzl2005@sina.com>
2024-06-07 13:58:26 +0800

    chore: 增加渲染等级示例并移除 composition 上的 renderLevel

---
+++ web-packages/demo/src/render-level.ts
@@ -0,0 +1,29 @@
+import { Player, spec } from '@galacean/effects';
+
+const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*GC99RbcyZiMAAAAAAAAAAAAADlB4AQ';
+const container = document.getElementById('J-container');
+const selectEle = document.getElementById('J-select') as HTMLSelectElement;
+
+(async () => {
+  try {
+    const player = new Player({
+      container,
+    });
+
+    await player.loadScene(json, {
+      renderLevel: spec.RenderLevel.BPlus,
+    });
+
+    // 切换机型
+    selectEle?.addEventListener('change', async () => {
+      const renderLevel = selectEle.value as spec.RenderLevel;
+
+      player.getCompositions().forEach(composition => composition.dispose());
+      await player.loadScene(json, {
+        renderLevel,
+      });
+    });
+  } catch (e) {
+    console.error('biz', e);
+  }
+})();
