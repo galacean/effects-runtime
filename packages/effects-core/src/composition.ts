6c9113865053e6d00013ef962e26322ca4ccc087
yiiqii <yfj5tzl2005@sina.com>
2024-06-14 18:46:44 +0800

    chore: 暴露 composition 部分属性和方法

--- packages/effects-core/src/composition.ts
+++ packages/effects-core/src/composition.ts
@@ -178,6 +178,8 @@ export class Composition implements Disposable, LostHandler {
    */
   protected readonly keepColorBuffer: boolean;
   protected rootComposition: CompositionComponent;
+  protected readonly postLoaders: Plugin[] = [];
+  protected compositionSourceManager: CompositionSourceManager;
 
   /**
    * 合成暂停/播放 标识
@@ -191,8 +193,6 @@ export class Composition implements Disposable, LostHandler {
   // private readonly event: EventSystem;
   // texInfo的类型有点不明确，改成<string, number>不会提前删除texture
   private readonly texInfo: Record<string, number>;
-  private readonly postLoaders: Plugin[] = [];
-  private compositionSourceManager: CompositionSourceManager;
 
   /**
    * Composition 构造函数
@@ -506,7 +506,7 @@ export class Composition implements Disposable, LostHandler {
     this.postLoaders.forEach(loader => loader.postProcessFrame(this, frame));
   }
 
-  private gatherRendererComponent (vfxItem: VFXItem, renderFrame: RenderFrame) {
+  protected gatherRendererComponent (vfxItem: VFXItem, renderFrame: RenderFrame) {
     for (const rendererComponent of vfxItem.rendererComponents) {
       if (rendererComponent.isActiveAndEnabled) {
         renderFrame.addMeshToDefaultRenderPass(rendererComponent);
@@ -733,7 +733,6 @@ export class Composition implements Disposable, LostHandler {
 
     // 视频固定30帧更新
     if (now - this.lastVideoUpdateTime > 33) {
-
       (this.textures ?? []).forEach(tex => tex?.uploadCurrentVideoFrame());
       this.lastVideoUpdateTime = now;
     }
