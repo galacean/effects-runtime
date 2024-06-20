68182a824f88380eadf85852564848b2f2c848d6
wumaolinmaoan <wumaolin1998@outlook.com>
2024-06-20 17:23:26 +0800

    perf: opt code

--- web-packages/imgui-demo/src/main.ts
+++ web-packages/imgui-demo/src/main.ts
@@ -105,14 +105,12 @@ async function _init (): Promise<void> {
 
   const fontConfig = new ImGui.FontConfig();
 
-  fontConfig.internal.OversampleH = 5;
-  fontConfig.internal.OversampleV = 5;
-  fontConfig.internal.RasterizerMultiply = 1.25;
-  font = await AddFontFromFileTTF('./public/fonts/AlibabaPuHuiTi-3-55-Regular.ttf', 22, fontConfig, io.Fonts.GetGlyphRangesChineseSimplifiedCommon());
+  fontConfig.internal.OversampleH = 2;
+  fontConfig.internal.OversampleV = 1;
+  font = await AddFontFromFileTTF('./public/fonts/AlibabaPuHuiTi-3-55-Regular.ttf', 44, fontConfig, io.Fonts.GetGlyphRangesChineseSimplifiedCommon());
   // font = await AddFontFromFileTTF("https://raw.githubusercontent.com/googlei18n/noto-cjk/master/NotoSansJP-Regular.otf", 18.0, null, io.Fonts.GetGlyphRangesJapanese());
   ImGui.ASSERT(font !== null);
-
-  // ImGui.GetStyle().ScaleAllSizes(1);
+  font.Scale = 0.5;
 
   // Setup Platform/Renderer backends
   // ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
