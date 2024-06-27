37caabb20c43813235480d87ccf6e15520f6fa59
liuxi150 <liuxi150@hotmail.com>
2024-06-27 20:46:47 +0800

    test: fix test case

--- plugin-packages/model/test/src/plugin-unit.spec.ts
+++ plugin-packages/model/test/src/plugin-unit.spec.ts
@@ -441,7 +441,7 @@ describe('渲染插件单测', function () {
     expect(cameraManager.cameraList).to.eql([cam1, cam2]);
     cameraManager.updateDefaultCamera(
       45, 0.001, 606, [30.5, 100, 77],
-      Quaternion.fromAxisAngle(new Vector3(10, -15, 33), 0.3, new Quaternion()), 0
+      Quaternion.fromAxisAngle(new Vector3(10, -15, 33), 0.3, new Quaternion()), 0, 0
     );
     const acam = cameraManager.getActiveCamera();
     expect(acam.width).to.eql(1024);
