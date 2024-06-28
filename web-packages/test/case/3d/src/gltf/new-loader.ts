9b7020cec097fa038caee917283626fd6864408a
liuxi150 <liuxi150@hotmail.com>
2024-06-28 13:47:39 +0800

    test: update frame comparison testing

--- web-packages/test/case/3d/src/gltf/new-loader.ts
+++ web-packages/test/case/3d/src/gltf/new-loader.ts
@@ -1,3 +1,99 @@
-import { loadGLTFSceneECS } from '../../../../../../plugin-packages/model/src/gltf/loader-ecs-ext';
+import { GLTFTools } from '@vvfx/resource-detection';
+import type { Player } from '@galacean/effects';
+import type { LoadSceneOptions, LoadSceneECSResult } from '@galacean/effects-plugin-model';
+import { spec } from '@galacean/effects';
+import { LoaderECSImpl } from '@galacean/effects-plugin-model';
+import { Box3, Vector3, Sphere } from '@galacean/effects-plugin-model';
 
-export { loadGLTFSceneECS };
+export class LoaderECSEx extends LoaderECSImpl {
+  override async loadScene (options: LoadSceneOptions): Promise<LoadSceneECSResult> {
+    const gltfResource = options.gltf.resource;
+
+    if (typeof gltfResource === 'string' || gltfResource instanceof Uint8Array) {
+      if (typeof gltfResource === 'string') {
+        console.info(`Load url: ${gltfResource}.`);
+      }
+
+      const gltfResult = await GLTFTools.loadGLTF(gltfResource);
+      const gltfDoc = gltfResult.doc;
+      const gltfJson = gltfResult.json;
+
+      options.gltf.remark = gltfResource;
+      options.gltf.resource = GLTFTools.processGLTFForEditorECS(gltfDoc, gltfJson);
+    }
+
+    return super.loadScene(options);
+  }
+}
+
+export interface LoadGLTFSceneECSOptions {
+  url: string,
+  player: Player,
+  playAnimation?: number,
+  playAllAnimation?: boolean,
+  camera?: {
+    position?: spec.vec3,
+    rotation?: spec.vec3,
+  },
+}
+
+export async function loadGLTFSceneECS (options: LoadGLTFSceneECSOptions) {
+  const duration = 9999;
+  const endBehavior = 5;
+  const loader = new LoaderECSEx();
+
+  return loader.loadScene({
+    gltf: {
+      resource: options.url,
+      compatibleMode: 'tiny3d',
+      skyboxType: 'NFT',
+      skyboxVis: true,
+    },
+    effects: {
+      renderer: options.player.renderer,
+      duration: duration,
+      endBehavior: endBehavior,
+      playAnimation: options.playAnimation,
+      playAllAnimation: options.playAllAnimation,
+    },
+  }).then(result => {
+    const sceneMin = Vector3.fromArray(result.sceneAABB.min);
+    const sceneMax = Vector3.fromArray(result.sceneAABB.max);
+    const sceneAABB = new Box3(sceneMin, sceneMax);
+    const sceneRadius = sceneAABB.getBoundingSphere(new Sphere()).radius;
+    const sceneCenter = sceneAABB.getCenter(new Vector3());
+    const position = sceneCenter.add(new Vector3(0, 0, sceneRadius * 1.71));
+    const cameraPosition = options.camera?.position ?? position.toArray();
+    const cameraRotation = options.camera?.rotation ?? [0, 0, 0];
+
+    loader.addCamera({
+      near: 0.2,
+      far: 500,
+      fov: 60,
+      clipMode: 0,
+      name: 'extra-camera',
+      endBehavior: 5,
+      duration: duration,
+      position: cameraPosition,
+      rotation: cameraRotation,
+    });
+
+    loader.addLight({
+      lightType: spec.LightType.ambient,
+      color: { r: 1, g: 1, b: 1, a: 1 },
+      intensity: 0.1,
+      //
+      name: 'env-light',
+      position: [0, 0, 0],
+      rotation: [0, 0, 0],
+      scale: [1, 1, 1],
+      duration: duration,
+      endBehavior: spec.ItemEndBehavior.loop,
+    });
+
+    const loadResult = loader.getLoadResult();
+
+    return loadResult.jsonScene;
+  });
+}
+
