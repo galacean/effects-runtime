cda049432cdaf2554750737683890cea94c33db1
茂安 <151805118+wumaolinmaoan@users.noreply.github.com>
2024-07-03 18:54:59 +0800

    feat: add camera fov ratio (#475)
    * feat: support camera fov scale ratio
    
    * perf: opt code
    
    * perf: opt code
    
    * perf: opt code
    
    * perf: opt code

--- packages/effects-core/src/composition.ts
+++ packages/effects-core/src/composition.ts
@@ -164,8 +164,6 @@ export class Composition implements Disposable, LostHandler {
    */
   globalTime: number;
 
-  editorScaleRatio = 1.0;
-
   protected rendererOptions: MeshRendererOptions | null;
   // TODO: 待优化
   protected assigned = false;
@@ -310,6 +308,14 @@ export class Composition implements Disposable, LostHandler {
    */
   get isDestroyed (): boolean {
     return this.destroyed;
+  }
+
+  set editorScaleRatio (value: number) {
+    this.camera.fovScaleRatio = value;
+  }
+
+  get editorScaleRatio () {
+    return this.camera.fovScaleRatio;
   }
 
   /**
--- packages/effects-core/src/plugins/particle/particle-system.ts
+++ packages/effects-core/src/plugins/particle/particle-system.ts
@@ -3,7 +3,6 @@ import { Euler, Matrix4, Vector2, Vector3, Vector4 } from '@galacean/effects-mat
 import type { vec2, vec3, vec4 } from '@galacean/effects-specification';
 import * as spec from '@galacean/effects-specification';
 import { Component } from '../../components';
-import { PLAYER_OPTIONS_ENV_EDITOR } from '../../constants';
 import { effectsClass } from '../../decorators';
 import type { Engine } from '../../engine';
 import type { ValueGetter } from '../../math';
@@ -645,11 +644,6 @@ export class ParticleSystem extends Component {
     const matrix4 = options.particleFollowParent ? this.transform.getMatrix() : this.transform.getWorldMatrix();
     const pointPosition: Vector3 = data.position;
 
-    if (this.item.composition?.renderer.env === PLAYER_OPTIONS_ENV_EDITOR) {
-      pointPosition.x /= this.item.composition.editorScaleRatio;
-      pointPosition.y /= this.item.composition.editorScaleRatio;
-    }
-
     // 粒子的位置受发射器的位置影响，自身的旋转和缩放不受影响
     const position = matrix4.transformPoint(pointPosition, new Vector3());
     const transform = new Transform({
@@ -743,11 +737,6 @@ export class ParticleSystem extends Component {
       size.x *= tempScale.x;
       size.y *= tempScale.y;
     }
-    if (this.item.composition?.renderer.env === PLAYER_OPTIONS_ENV_EDITOR) {
-      size.x /= this.item.composition.editorScaleRatio;
-      size.y /= this.item.composition.editorScaleRatio;
-    }
-
     transform.setScale(size.x, size.y, 1);
 
     return {
