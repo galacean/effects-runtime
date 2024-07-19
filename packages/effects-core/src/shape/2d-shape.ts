507e2f58229911f6ed2c2d35a4b02dc8b01d637d
wumaolinmaoan <wumaolin1998@outlook.com>
2024-07-19 15:59:03 +0800

    fix: particle edge shape

--- packages/effects-core/src/shape/2d-shape.ts
+++ packages/effects-core/src/shape/2d-shape.ts
@@ -98,8 +98,7 @@ export class Edge implements Shape {
   _d: number;
 
   constructor (args: any) {
-    // TODO: 为通过帧对比暂时使用老计算，修复粒子发射器直线宽度问题下面一行改为: this._d = args.width || 1 ;
-    this._d = (args.width || 1) / 2;
+    this._d = (args.width || 1);
     this.arcMode = args.arcMode;
   }
 
