d4bd6bee1cb3dc30d5c5c76a16ccdce95788295e
SuRuiMeng <934274351@qq.com>
2024-08-01 11:00:23 +0800

    feat: 增加 动态换图 单测

--- web-packages/test/unit/src/effects-core/index.ts
+++ web-packages/test/unit/src/effects-core/index.ts
@@ -1,16 +1,17 @@
-import './composition';
-// import './fallback';
-// import './image-template/image-template.spec';
-// import './interact/interact.spec';
-// import './math';
-// import './plugins/cal/transform.spec';
-// import './plugins/common/end-behevior.spec';
-// // import './plugins/common/downgrade.spec';
-// // plugin particle
-// import './plugins/particle';
-// // plugin sprite
-import './plugins/sprite';
-// import './assert-manager.spec';
-import './texture.spec';
-import './transform.spec';
-import './utils.spec';
+// // import './composition';
+// // import './fallback';
+// // import './image-template/image-template.spec';
+// // import './interact/interact.spec';
+// // import './math';
+// // import './plugins/cal/transform.spec';
+// // import './plugins/common/end-behevior.spec';
+// // // import './plugins/common/downgrade.spec';
+// // // plugin particle
+// // import './plugins/particle';
+// // // plugin sprite
+// import './plugins/sprite';
+// // import './assert-manager.spec';
+// import './texture.spec';
+// import './transform.spec';
+// import './utils.spec';
+import './template-image.spec';
