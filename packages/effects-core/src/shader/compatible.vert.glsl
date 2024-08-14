91e69fd2b37f7b23abee9b301d2e7d6a7ffdc69a
liuxi150 <liuxi150@hotmail.com>
2024-08-14 10:58:42 +0800

    fix: update

--- packages/effects-core/src/shader/compatible.vert.glsl
+++
@@ -1,5 +0,0 @@
-#version 300 es
-#ifdef WEBGL2
-#define texture2D texture
-#else
-#endif
--- plugin-packages/model/src/runtime/shader-libs/standard/webgl-compatibility.glsl
+++
@@ -1,5 +0,0 @@
-#ifdef WEBGL2
-    #define texture2D texture
-    #define textureCube texture
-    #define textureCubeLodEXT textureLod
-#endif
