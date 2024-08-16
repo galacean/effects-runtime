90445d6f6c9b29a22de8ad9acb7f4a0d867adffe
wumaolinmaoan <wumaolin1998@outlook.com>
2024-08-16 17:52:31 +0800

    perf: opt code

--- packages/effects-core/src/plugins/particle/particle-system.ts
+++ packages/effects-core/src/plugins/particle/particle-system.ts
@@ -337,13 +337,6 @@ export class ParticleSystem extends Component {
     this.ended = false;
   }
 
-  override onAttached (): void {
-    super.onAttached();
-    this.renderer.item = this.item;
-    this.item.components.push(this.renderer);
-    this.item.rendererComponents.push(this.renderer);
-  }
-
   onUpdate (delta: number) {
     if (this.started && !this.frozen) {
       const now = this.lastUpdate + delta / 1000;
@@ -848,6 +841,13 @@ export class ParticleSystem extends Component {
       };
     }
   };
+
+  override onAttached (): void {
+    super.onAttached();
+    this.renderer.item = this.item;
+    this.item.components.push(this.renderer);
+    this.item.rendererComponents.push(this.renderer);
+  }
 
   override fromData (data: unknown): void {
     super.fromData(data);
