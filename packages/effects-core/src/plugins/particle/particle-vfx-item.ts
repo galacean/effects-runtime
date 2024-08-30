65dd0f6b2d6a48210d7617db0f9efb5ff3607a4a
wumaolinmaoan <wumaolin1998@outlook.com>
2024-08-30 19:36:52 +0800

    fix: particle render on composition restart

--- packages/effects-core/src/plugins/particle/particle-vfx-item.ts
+++ packages/effects-core/src/plugins/particle/particle-vfx-item.ts
@@ -34,10 +34,10 @@ export class ParticleBehaviourPlayable extends Playable {
     if (particleSystem) {
       // TODO: [1.31] @十弦 验证 https://github.com/galacean/effects-runtime/commit/3e7d73d37b7d98c2a25e4544e80e928b17801ccd#diff-fae062f28caf3771cfedd3a20dc22f9749bd054c7541bf2fd50a9a5e413153d4
       // particleSystem.setParentTransform(parentItem.transform);
-      particleSystem.setVisible(true);
+      // particleSystem.setVisible(true);
       let deltaTime = context.deltaTime;
 
-      if (this.time < particleSystem.item.duration && particleSystem.isFrozen()) {
+      if (this.time >= 0 && this.time < particleSystem.item.duration && particleSystem.isEnded()) {
         particleSystem.reset();
       }
       if (Math.abs(this.time - this.lastTime) < 0.001) {
