0c606f0c998be88db813b748a0ff4675654297b1
Sruim <934274351@qq.com>
2024-09-03 12:23:33 +0800

    faet: add Texture class fromImage options (#622)
    * feat: 增加 Texture fromImage options 参数
    
    * test: 增加 fromImage 参数单测

--- web-packages/test/unit/src/effects-core/texture.spec.ts
+++ web-packages/test/unit/src/effects-core/texture.spec.ts
@@ -1,4 +1,4 @@
-import { Texture, Player } from '@galacean/effects';
+import { Texture, Player, glContext } from '@galacean/effects';
 
 const { expect } = chai;
 
@@ -25,6 +25,28 @@ describe('core/texture', () => {
   it('texture from image', async () => {
     const testTexture = await Texture.fromImage('https://gw.alipayobjects.com/mdn/rms_2e421e/afts/img/A*fRtNTKrsq3YAAAAAAAAAAAAAARQnAQ', player.renderer.engine);
 
+    expect(testTexture).to.be.an.instanceOf(Texture);
+    expect(testTexture.width).to.be.a('number');
+    expect(testTexture.height).to.be.a('number');
+    expect(testTexture.id).to.be.a('string');
+  });
+
+  it('texture from image with options', async () => {
+    const testTexture = await Texture.fromImage('https://gw.alipayobjects.com/mdn/rms_2e421e/afts/img/A*fRtNTKrsq3YAAAAAAAAAAAAAARQnAQ', player.renderer.engine,
+      {
+        minFilter: glContext.LINEAR_MIPMAP_LINEAR,
+        magFilter: glContext.LINEAR,
+        wrapS: glContext.REPEAT,
+        wrapT: glContext.MIRRORED_REPEAT,
+      }
+    );
+    const textureSource = testTexture.source;
+
+    expect(textureSource.minFilter).to.equal(glContext.LINEAR_MIPMAP_LINEAR);
+    expect(textureSource.magFilter).to.equal(glContext.LINEAR);
+    expect(textureSource.wrapS).to.equal(glContext.REPEAT);
+    expect(textureSource.wrapT).to.equal(glContext.MIRRORED_REPEAT);
+    expect(textureSource.flipY).to.be.true;
     expect(testTexture).to.be.an.instanceOf(Texture);
     expect(testTexture.width).to.be.a('number');
     expect(testTexture.height).to.be.a('number');
