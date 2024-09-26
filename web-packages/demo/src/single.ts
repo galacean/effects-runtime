90d956454ec4f9edafc51b8d935caab6f1ec1de8
SuRuiMeng <934274351@qq.com>
2024-09-26 16:21:26 +0800

    feat(demo): 增加交互功能

--- web-packages/demo/src/single.ts
+++ web-packages/demo/src/single.ts
@@ -1,19 +1,18 @@
 import { Player } from '@galacean/effects';
 import '@galacean/effects-plugin-spine';
 
-const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*L806QJv14k0AAAAAAAAAAAAADlB4AQ';
+const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*WXEqSadfOKcAAAAAAAAAAAAADlB4AQ'
 const container = document.getElementById('J-container');
 
 (async () => {
   try {
     const player = new Player({
       container,
-
+      interactive: true,
     });
 
-    await player.loadScene(json, {
-      reusable: true,
-    });
+    await player.loadScene(json);
+
   } catch (e) {
     console.error('biz', e);
   }
