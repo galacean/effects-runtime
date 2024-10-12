cbf07c5fd4b7b1d4f8256cb8dec102aa9cd14a2b
SuRuiMeng <934274351@qq.com>
2024-10-12 14:57:59 +0800

    chore(media): 修改自动播放检测逻辑

--- plugin-packages/multimedia/src/audio/audio-loader.ts
+++ plugin-packages/multimedia/src/audio/audio-loader.ts
@@ -1,13 +1,12 @@
 import type { SceneLoadOptions } from '@galacean/effects';
 import { spec, AbstractPlugin } from '@galacean/effects';
-import { checkAutoplayPermission, processMultimedia } from '../utils';
+import { processMultimedia } from '../utils';
 
 export class AudioLoader extends AbstractPlugin {
   static override async prepareAssets (
     json: spec.JSONScene,
     options: SceneLoadOptions = {},
   ) {
-    await checkAutoplayPermission();
     const { audios = [] } = json;
     const loadedAssets = await processMultimedia<HTMLAudioElement | AudioBuffer>(audios, spec.MultimediaType.audio, options);
 
--- plugin-packages/multimedia/src/video/video-loader.ts
+++ plugin-packages/multimedia/src/video/video-loader.ts
@@ -1,13 +1,12 @@
 import type { SceneLoadOptions } from '@galacean/effects';
 import { spec, AbstractPlugin } from '@galacean/effects';
-import { processMultimedia, checkAutoplayPermission } from '../utils';
+import { processMultimedia } from '../utils';
 
 export class VideoLoader extends AbstractPlugin {
   static override async prepareAssets (
     json: spec.JSONScene,
     options: SceneLoadOptions = {},
   ) {
-    await checkAutoplayPermission();
     const { videos = [] } = json;
     const loadedAssets = await processMultimedia<HTMLVideoElement>(videos, spec.MultimediaType.video, options);
 
