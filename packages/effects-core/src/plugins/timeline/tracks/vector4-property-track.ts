9b061f7fba8bf3cca9cacb87853b1df43baa4359
wumaolinmaoan <wumaolin1998@outlook.com>
2024-10-18 17:28:06 +0800

    feat: add vector4 property mixer

---
+++ packages/effects-core/src/plugins/timeline/tracks/vector4-property-track.ts
@@ -0,0 +1,21 @@
+import { effectsClass } from '../../../decorators';
+import type { PlayableGraph, Playable } from '../../cal/playable-graph';
+import { Vector4PropertyMixerPlayable } from '../playables/vector4-property-mixer-playable';
+import { PropertyTrack } from './property-track';
+
+@effectsClass('Vector4PropertyTrack')
+export class Vector4PropertyTrack extends PropertyTrack {
+  override createTrackMixer (graph: PlayableGraph): Playable {
+    const mixer = new Vector4PropertyMixerPlayable(graph);
+
+    const propertyNames = this.propertyNames;
+
+    if (propertyNames.length > 0) {
+      const propertyName = propertyNames[propertyNames.length - 1];
+
+      mixer.propertyName = propertyName;
+    }
+
+    return mixer;
+  }
+}
