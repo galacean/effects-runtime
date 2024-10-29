46d0216748842bbdce25a4a6e3d8a234a7cf1ec2
SuRuiMeng <934274351@qq.com>
2024-10-29 11:38:35 +0800

    refactor: 优化富文本插件名称和相关代码

--- plugin-packages/rich-text/src/rich-text-component.ts
+++ plugin-packages/rich-text/src/rich-text-component.ts
@@ -1,32 +1,26 @@
 import type { Engine } from '@galacean/effects';
 import {
+  effectsClass,
+  glContext,
+  spec,
+  TextComponent,
   Texture } from '@galacean/effects';
 import {
   TextLayout,
   TextStyle,
 } from '@galacean/effects';
-import { spec, effectsClass, BaseRenderComponent, glContext, canvasPool } from '@galacean/effects';
 import { generateProgram } from './rich-text-parser';
-import { RichTextOptions } from './rich-text-options';
 import { ColorUtils } from './color-utils';
 
-/**
- * 用于创建 textItem 的数据类型, 经过处理后的 spec.TextContentOptions
- */
-
-export interface RichTextItemProps extends Omit<RichtextOptions, 'renderer'> {
-  interaction?: {
-    /**
-     * 交互行为
-     */
-    behavior: spec.InteractBehavior,
-  },
-  options: RichtextOptions,
-  listIndex?: number,
-  renderer: {
-    mask: number,
-    texture: Texture,
-  } & Omit<spec.RendererOptions, 'texture'>,
+export interface RichTextOptions {
+  text: string,
+  fontSize: number,
+  fontFamily?: string,
+  fontWeight?: spec.TextWeight,
+  fontStyle?: spec.FontStyle,
+  fontColor?: spec.vec4,
+  textStyle?: TextStyle,
+  isNewLine: boolean,
 }
 
 interface RichCharInfo {
@@ -49,66 +43,27 @@ interface RichCharInfo {
   fontHeight: number,
 }
 
-export interface RichtextOptions {
-  text: string,
-  fontSize: number,
-  fontFamily: string,
-  textAlign: spec.TextAlignment,
-  fontWeight: spec.TextWeight,
-  fontStyle: spec.FontStyle,
-  textWidth: number,
-  textHeight: number,
-  textColor: spec.vec4,
-}
-
 let seed = 0;
 
-@effectsClass('RichTextComponent')
-export class RichTextComponent extends BaseRenderComponent {
-  canvas: HTMLCanvasElement;
-  context: CanvasRenderingContext2D | null;
-  textStyle: TextStyle;
-  textLayout: TextLayout;
+@effectsClass(spec.DataType.RichTextComponent)
+export class RichTextComponent extends TextComponent {
   processedTextOptions: RichTextOptions[] = [];
-  isDirty: boolean = true;
   private singleLineHeight: number = 1.571;
   constructor (engine: Engine) {
     super(engine);
     this.name = 'MRichText' + seed++;
-    this.geometry = this.createGeometry(glContext.TRIANGLES);
-    this.canvas = canvasPool.getCanvas();
-    canvasPool.saveCanvas(this.canvas);
-    this.context = this.canvas.getContext('2d', { willReadFrequently: true });
-    this.setItem();
   }
 
-  override fromData (data: RichTextItemProps): void {
-    super.fromData(data);
-    const { interaction, options, listIndex = 0 } = data;
-    let renderer = data.renderer;
-
-    if (!renderer) {
-      renderer = {} as any;
-    }
-    this.interaction = interaction;
-    this.updateWithOptions(options);
-
-    this.renderer = {
-      renderMode: renderer.renderMode ?? spec.RenderMode.BILLBOARD,
-      blending: renderer.blending ?? spec.BlendingMode.ALPHA,
-      texture: renderer.texture ?? this.engine.emptyTexture,
-      occlusion: !!renderer.occlusion,
-      transparentOcclusion: !!renderer.transparentOcclusion || (renderer.maskMode === spec.MaskMode.MASK),
-      side: renderer.side ?? spec.SideMode.DOUBLE,
-      mask: renderer.mask ?? 0,
-      maskMode: renderer.maskMode ?? spec.MaskMode.NONE,
-      order: listIndex,
-    };
+  private generateTextProgram (text: string) {
     const program = generateProgram((text, context) => {
       const textArr = text.split('\n');
 
       textArr.forEach((text, index) => {
-        const options = new RichTextOptions(text, this.textStyle.fontSize);
+        const options: RichTextOptions = {
+          text,
+          fontSize: this.textStyle.fontSize,
+          isNewLine: false,
+        };
 
         if (index > 0) {
           options.isNewLine = true;
@@ -133,14 +88,14 @@ export class RichTextComponent extends BaseRenderComponent {
 
     });
 
-    program(options.text);
-    this.updateTexture();
+    program(text);
   }
 
-  updateTexture (flipY = true) {
+  override updateTexture (flipY = true) {
     if (!this.isDirty || !this.context || !this.canvas) {
       return;
     }
+    this.generateTextProgram(this.text);
     let width = 0, height = 0;
     const { textLayout, textStyle } = this;
 
@@ -242,9 +197,10 @@ export class RichTextComponent extends BaseRenderComponent {
     this.isDirty = false;
   }
 
-  updateWithOptions (options: spec.TextContentOptions) {
+  override updateWithOptions (options: spec.TextContentOptions) {
     this.textStyle = new TextStyle(options);
     this.textLayout = new TextLayout(options);
+    this.text = options.text.toString();
   }
 
 }
