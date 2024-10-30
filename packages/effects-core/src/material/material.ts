e82edd0861afa58d97e7d4be8285e287e6cd96c8
wumaolinmaoan <wumaolin1998@outlook.com>
2024-10-30 17:09:48 +0800

    refactor: opt shader variant create logic

--- packages/effects-core/src/material/material.ts
+++ packages/effects-core/src/material/material.ts
@@ -62,7 +62,6 @@ let seed = 1;
  * Material 抽象类
  */
 export abstract class Material extends EffectsObject implements Disposable {
-  shader: Shader;
   shaderVariant: ShaderVariant;
 
   // TODO: 待移除
@@ -76,6 +75,9 @@ export abstract class Material extends EffectsObject implements Disposable {
 
   protected destroyed = false;
   protected initialized = false;
+  protected shaderDirty = true;
+
+  private _shader: Shader;
 
   /**
    *
@@ -104,6 +106,15 @@ export abstract class Material extends EffectsObject implements Disposable {
       this.name = 'Material' + seed++;
       this.renderType = MaterialRenderType.normal;
     }
+  }
+
+  get shader () {
+    return this._shader;
+  }
+
+  set shader (value: Shader) {
+    this._shader = value;
+    this.shaderDirty = true;
   }
 
   /******** effects-core 中会调用 引擎必须实现 ***********************/
