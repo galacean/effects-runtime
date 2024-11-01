04b995e7db02834b594b4af6cab558e7b42f1f93
SuRuiMeng <934274351@qq.com>
2024-11-01 11:39:48 +0800

    feat: add autoplay permission check in audio and video loaders

--- plugin-packages/multimedia/src/video/video-loader.ts
+++ plugin-packages/multimedia/src/video/video-loader.ts
@@ -1,11 +1,25 @@
-import type { SceneLoadOptions } from '@galacean/effects';
+import type { PrecompileOptions, Renderer, SceneLoadOptions } from '@galacean/effects';
 import { spec, AbstractPlugin } from '@galacean/effects';
-import { processMultimedia } from '../utils';
+import { checkAutoplayPermission, processMultimedia } from '../utils';
 
 /**
  * 视频加载插件
  */
 export class VideoLoader extends AbstractPlugin {
+
+  static override precompile (compositions: spec.Composition[], renderer: Renderer, options?: PrecompileOptions): Promise<any> {
+    const engine = renderer.engine;
+    const { env } = options ?? {};
+
+    if (env === 'editor') {
+      checkAutoplayPermission().catch(e => {
+        engine.renderErrors.add(e);
+      });
+    }
+
+    return Promise.resolve();
+  }
+
   static override async processAssets (
     json: spec.JSONScene,
     options: SceneLoadOptions = {},
