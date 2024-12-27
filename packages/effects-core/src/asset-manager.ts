de402e1fd10759f14604a876eceadc55784216f8
陈榕 <38219632+RGCHN@users.noreply.github.com>
2024-12-27 09:52:20 +0800

    feat: 增加是否禁用 avif/webp 参数 (#805)
    * feat: 增加是否使用avif/webp参数
    
    * fix: 专有名称大小写问题
    
    ---------
    
    Co-authored-by: yiiqii <yfj5tzl2005@sina.com>

--- packages/effects-core/src/asset-manager.ts
+++ packages/effects-core/src/asset-manager.ts
@@ -278,16 +278,16 @@ export class AssetManager implements Disposable {
     images: spec.ImageSource[],
     compressedTexture: COMPRESSED_TEXTURE = 0,
   ): Promise<ImageLike[]> {
-    const { useCompressedTexture, variables } = this.options;
+    const { useCompressedTexture, variables, disableWebP, disableAVIF } = this.options;
     const baseUrl = this.baseUrl;
     const jobs = images.map(async (img, idx: number) => {
       const { url: png, webp, avif } = img;
       // eslint-disable-next-line compat/compat
       const imageURL = new URL(png, baseUrl).href;
       // eslint-disable-next-line compat/compat
-      const webpURL = webp && new URL(webp, baseUrl).href;
+      const webpURL = (!disableWebP && webp) ? new URL(webp, baseUrl).href : undefined;
       // eslint-disable-next-line compat/compat
-      const avifURL = avif && new URL(avif, baseUrl).href;
+      const avifURL = (!disableAVIF && avif) ? new URL(avif, baseUrl).href : undefined;
 
       const id = img.id;
 
