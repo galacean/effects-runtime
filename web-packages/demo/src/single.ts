610c76998d64d84beb2d92c87d5619d7bca11d15
RGCHN <dacong9610@163.com>
2025-04-03 14:41:26 +0800

    fix: 修复 Spine 蒙版旧兼容

--- web-packages/demo/src/single.ts
+++ web-packages/demo/src/single.ts
@@ -1,7 +1,9 @@
 import { Player } from '@galacean/effects';
 import '@galacean/effects-plugin-spine';
+import inspireList from './assets/inspire-list';
 
-const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*vO0wT4S4shEAAAAAAAAAAAAAelB4AQ';
+const json = inspireList.midAutumn;
+// 'https://mdn.alipayobjects.com/mars/afts/file/A*vO0wT4S4shEAAAAAAAAAAAAAelB4AQ';
 const container = document.getElementById('J-container');
 
 (async () => {
