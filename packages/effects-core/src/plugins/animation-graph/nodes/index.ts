ab8e0f4df9166d1ace20bcf2607215ea8004967a
wumaolinmaoan <wumaolin1998@outlook.com>
2025-05-26 15:23:03 +0800

    chore: update not node export

--- packages/effects-core/src/plugins/animation-graph/nodes/index.ts
+++ packages/effects-core/src/plugins/animation-graph/nodes/index.ts
@@ -1,6 +1,7 @@
 export * from './animation-clip-node';
 export * from './animation-root-node';
 export * from './blend-1d-node';
+export * from './bool-nodes';
 export * from './const-value-nodes';
 export * from './control-parameter-nodes';
 export * from './state-machine-node';
