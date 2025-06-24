253a9f4251e5758b58bb3fdddcae16241917bc73
yiiqii <yfj5tzl2005@sina.com>
2025-06-24 19:40:38 +0800

    test: 补充富文本字体遮挡的帧对比 case

--- plugin-packages/rich-text/src/rich-text-component.ts
+++ plugin-packages/rich-text/src/rich-text-component.ts
@@ -42,7 +42,7 @@ let seed = 0;
 @effectsClass(spec.DataType.RichTextComponent)
 export class RichTextComponent extends TextComponent {
   processedTextOptions: RichTextOptions[] = [];
-  //字体高度的倍数（字体高度上下多出来的部分就是行间距）
+  // 字体高度的倍数（字体高度上下多出来的部分就是行间距）
   private singleLineHeight: number = 1.571;
   private size: math.Vector2 | null = null;
   /**
@@ -63,7 +63,7 @@ export class RichTextComponent extends TextComponent {
   private generateTextProgram (text: string) {
     this.processedTextOptions = [];
     const program = generateProgram((text, context) => {
-      // 如果富文本仅包含换行符，则在每个换行符后添加一个空格
+      //  如果富文本仅包含换行符，则在每个换行符后添加一个空格
       if (/^\n+$/.test(text)) {
         text = text.replace(/\n/g, '\n ');
       }
@@ -106,7 +106,7 @@ export class RichTextComponent extends TextComponent {
     if (!this.isDirty || !this.context || !this.canvas) {
       return;
     }
-    //解析富文本
+    // 解析富文本
     this.generateTextProgram(this.text);
     let width = 0, height = 0;
     const { textLayout, textStyle } = this;
@@ -121,17 +121,17 @@ export class RichTextComponent extends TextComponent {
       richOptions: [],
       offsetX: [],
       width: 0,
-      //包括字体和上下行间距的高度
+      // 包括字体和上下行间距的高度
       lineHeight: fontHeight * this.singleLineHeight,
-      //字体偏移高度（也就是行间距）
+      // 字体偏移高度（也就是行间距）
       offsetY: fontHeight * (this.singleLineHeight - 1) / 2,
     };
 
-    //遍历解析后的文本选项
+    // 遍历解析后的文本选项
     this.processedTextOptions.forEach(options => {
       const { text, isNewLine, fontSize } = options;
 
-      //如果是新行，则将之前行的信息存入charsInfo并且初始化新行的charInfo
+      // 如果是新行，则将之前行的信息存入charsInfo并且初始化新行的charInfo
       if (isNewLine) {
         charsInfo.push(charInfo);
         width = Math.max(width, charInfo.width);
@@ -142,12 +142,12 @@ export class RichTextComponent extends TextComponent {
           lineHeight: fontHeight * this.singleLineHeight,
           offsetY: fontHeight * (this.singleLineHeight - 1) / 2,
         };
-        //管理行的总高度调整canvas尺寸
+        // 管理行的总高度调整canvas尺寸
         height += charInfo.lineHeight;
       }
-      //恢复默认设置
+      // 恢复默认设置
       context.font = `${options.fontWeight || textStyle.textWeight} 10px ${options.fontFamily || textStyle.fontFamily}`;
-      //计算得到每个字段的宽度textWidth
+      // 计算得到每个字段的宽度textWidth
       const textMetrics = context.measureText(text);
       let textWidth = textMetrics.width;
 
@@ -166,13 +166,13 @@ export class RichTextComponent extends TextComponent {
         charInfo.offsetY = fontSize * this.textStyle.fontScale * (this.singleLineHeight - 1) / 2;
       }
       charInfo.offsetX.push(charInfo.width);
-      //计算字段宽度*富文本字体大小*缩放因子*整体文本大小+每个字符的间距（每个字符间距存疑，因为实际测量的宽度已经包含了间距）
+      // 计算字段宽度*富文本字体大小*缩放因子*整体文本大小+每个字符的间距（每个字符间距存疑，因为实际测量的宽度已经包含了间距）
       charInfo.width += (textWidth <= 0 ? 0 : textWidth) * fontSize * this.SCALE_FACTOR * this.textStyle.fontScale + text.length * letterSpace;
 
-      //将富文本数据存入charInfo
+      // 将富文本数据存入charInfo
       charInfo.richOptions.push(options);
     });
-    //存储最后一行的字符信息，并且更新最终的宽度和高度用于确定canvas尺寸
+    // 存储最后一行的字符信息，并且更新最终的宽度和高度用于确定canvas尺寸
     charsInfo.push(charInfo);
     width = Math.max(width, charInfo.width);
     height += charInfo.lineHeight;
@@ -187,7 +187,7 @@ export class RichTextComponent extends TextComponent {
     }
     const { x = 1, y = 1 } = this.size;
 
-    //首次渲染时初始化canvas尺寸和组件变换
+    // 首次渲染时初始化canvas尺寸和组件变换
     if (!this.initialized) {
       this.canvasSize = !this.canvasSize ? new math.Vector2(width, height) : this.canvasSize;
       const { x: canvasWidth, y: canvasHeight } = this.canvasSize;
@@ -256,7 +256,7 @@ export class RichTextComponent extends TextComponent {
 
       });
     });
-    //与 toDataURL() 两种方式都需要像素读取操作
+    // 与 toDataURL() 两种方式都需要像素读取操作
     const imageData = context.getImageData(0, 0, this.canvas.width, this.canvas.height);
 
     const texture = Texture.createWithData(
