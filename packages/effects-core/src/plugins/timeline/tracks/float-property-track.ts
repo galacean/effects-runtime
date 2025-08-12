b616a8354ca025227bb2e44116cfb6ccb1090cab
wumaolinmaoan <wumaolin1998@outlook.com>
2025-08-12 15:09:59 +0800

    fix: float property track bound object issue

--- packages/effects-core/src/plugins/timeline/tracks/float-property-track.ts
+++ packages/effects-core/src/plugins/timeline/tracks/float-property-track.ts
@@ -17,18 +17,6 @@ export class FloatPropertyTrack extends PropertyTrack {
   }
 
   override updateAnimatedObject () {
-    const propertyNames = this.propertyNames;
-    let target: Record<string, any> = this.parent.boundObject;
-
-    for (let i = 0; i < propertyNames.length - 1; i++) {
-      const property = target[propertyNames[i]];
-
-      if (property === undefined) {
-        console.error('The ' + propertyNames[i] + ' property of ' + target + ' was not found');
-      }
-      target = property;
-    }
-
-    this.boundObject = target;
+    this.boundObject = this.parent.boundObject;
   }
 }
