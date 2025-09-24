89d81ace50095a01734bc3aaccddf287296bcb29
yiiqii <yfj5tzl2005@sina.com>
2025-09-24 17:55:27 +0800

    test: add cases

--- web-packages/test/case/src/2d/assets/interact.ts
+++ web-packages/test/case/src/2d/assets/interact.ts
@@ -1,15 +1,31 @@
 export default {
-  boom3: {
-    url: 'https://gw.alipayobjects.com/os/gltf-asset/mars-cli/LBGBIMBQOABC/-1154432351-de5ec.json',
-    name: '年兽炸弹——爆竹爆炸level3',
+  '11qKWVFMRk': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*NIyXQIg8mNgAAAAAQdAAAAgAelB4AQ',
+    name: '神秘的joker_交互动效',
   },
-  multiPeople: {
-    url: 'https://gw.alipayobjects.com/os/gltf-asset/mars-cli/ETELKDEUCNBC/2073953335-fd050.json',
-    name: '多人红包',
+  '11qKWVFMNj': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*sq53SZTqNQkAAAAAR7AAAAgAelB4AQ',
+    name: '可交互动效_抽奖机',
   },
-  invest: {
-    url: 'https://gw.alipayobjects.com/os/gltf-asset/mars-cli/PBVALWIQDUTE/-1454659523-382de.json',
-    name: '投资大咖',
+  '11qrALn2YM': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*icxwT4dYyBIAAAAASIAAAAgAelB4AQ',
+    name: '可爱的抽卡交互东西',
+  },
+  '12HKoY8JVR': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*2caCTZHwcMgAAAAARXAAAAgAelB4AQ',
+    name: '填诗词抓娃娃',
+  },
+  '12IGtt2Xnp': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*xs6ZS5e8pk4AAAAARYAAAAgAelB4AQ',
+    name: '抽奖转盘',
+  },
+  '12IZPDlHbX': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*fyfgTZ8x6r0AAAAAQ_AAAAgAelB4AQ',
+    name: '互动弹窗_抽奖机',
+  },
+  '12JjbNEr0r': {
+    url: 'https://mdn.alipayobjects.com/mars/afts/file/A*M-F0TqmzJ3oAAAAAUFAAAAgAelB4AQ',
+    name: '幸运翻牌互动小游戏',
   },
   camera2: {
     url: 'https://mdn.alipayobjects.com/mars/afts/file/A*4p4DQblt4PUAAAAAAAAAAAAADlB4AQ',
