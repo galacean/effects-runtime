3d707c8df8cba1f1da0b688d6b76d158d7b897f1
wumaolinmaoan <wumaolin1998@outlook.com>
2025-09-25 14:36:50 +0800

    refactor: usage of item duration and start

--- packages/effects-core/src/plugins/timeline/tracks/object-binding-track.ts
+++ packages/effects-core/src/plugins/timeline/tracks/object-binding-track.ts
@@ -17,58 +17,57 @@ import { ParticleTrack } from './particle-track';
 export class ObjectBindingTrack extends TrackAsset {
 
   create (timelineAsset: TimelineAsset, sceneBindingMap: Record<string, VFXItem>): void {
-
     const boundItem = sceneBindingMap[this.getInstanceId()];
 
     if (!(boundItem instanceof VFXItem)) {
       return;
     }
 
-    let hasActiveTrack = false;
-
     for (const childTrack of this.getChildTracks()) {
       if (childTrack instanceof ActivationTrack) {
-        hasActiveTrack = true;
+
+        // 添加粒子动画 clip // TODO 待移除
+        if (boundItem.getComponent(ParticleSystem)) {
+          const particleTrack = timelineAsset.createTrack(ParticleTrack, this, 'ParticleTrack');
+
+          for (const activationClip of childTrack.getClips()) {
+            const particleClip = particleTrack.createClip(ParticleBehaviourPlayableAsset);
+
+            particleClip.start = activationClip.start;
+            particleClip.duration = activationClip.duration;
+            particleClip.endBehavior = activationClip.endBehavior;
+          }
+
+        }
+
+        // 添加图层帧动画动画时间 clip // TODO 待移除
+        if (boundItem.getComponent(SpriteComponent)) {
+          const componentTimeTrack = timelineAsset.createTrack(SpriteComponentTimeTrack, this, 'SpriteComponentTimeTrack');
+
+          for (const activationClip of childTrack.getClips()) {
+            const clip = componentTimeTrack.createClip(ComponentTimePlayableAsset);
+
+            clip.start = activationClip.start;
+            clip.duration = activationClip.duration;
+            clip.endBehavior = activationClip.endBehavior;
+          }
+        }
+
+        // 添加图层帧动画动画时间 clip // TODO 待移除
+        if (boundItem.getComponent(EffectComponent)) {
+          const componentTimeTrack = timelineAsset.createTrack(EffectComponentTimeTrack, this, 'EffectComponentTimeTrack');
+
+          for (const activationClip of childTrack.getClips()) {
+            const clip = componentTimeTrack.createClip(ComponentTimePlayableAsset);
+
+            clip.start = activationClip.start;
+            clip.duration = activationClip.duration;
+            clip.endBehavior = activationClip.endBehavior;
+          }
+        }
 
         break;
       }
-    }
-
-    if (!hasActiveTrack) {
-      return;
-    }
-
-    // 添加粒子动画 clip // TODO 待移除
-    if (boundItem.getComponent(ParticleSystem)) {
-      const particleTrack = timelineAsset.createTrack(ParticleTrack, this, 'ParticleTrack');
-
-      const particleClip = particleTrack.createClip(ParticleBehaviourPlayableAsset);
-
-      particleClip.start = boundItem.start;
-      particleClip.duration = boundItem.duration;
-      particleClip.endBehavior = boundItem.endBehavior;
-    }
-
-    // 添加图层帧动画动画时间 clip // TODO 待移除
-    if (boundItem.getComponent(SpriteComponent)) {
-      const componentTimeTrack = timelineAsset.createTrack(SpriteComponentTimeTrack, this, 'SpriteComponentTimeTrack');
-
-      const clip = componentTimeTrack.createClip(ComponentTimePlayableAsset);
-
-      clip.start = boundItem.start;
-      clip.duration = boundItem.duration;
-      clip.endBehavior = boundItem.endBehavior;
-    }
-
-    // 添加图层帧动画动画时间 clip // TODO 待移除
-    if (boundItem.getComponent(EffectComponent)) {
-      const componentTimeTrack = timelineAsset.createTrack(EffectComponentTimeTrack, this, 'EffectComponentTimeTrack');
-
-      const clip = componentTimeTrack.createClip(ComponentTimePlayableAsset);
-
-      clip.start = boundItem.start;
-      clip.duration = boundItem.duration;
-      clip.endBehavior = boundItem.endBehavior;
     }
   }
 }
