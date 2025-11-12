293b2939a4f418910c1c069cd7032ce9c097db18
wumaolinmaoan <wumaolin1998@outlook.com>
2025-11-12 19:44:36 +0800

    chore: update

--- packages/effects-core/src/transform.ts
+++ packages/effects-core/src/transform.ts
@@ -500,9 +500,21 @@ export class Transform implements Disposable {
       tempVector3.applyQuaternion(tempQuat);
 
       // 应用逆缩放
-      tempVector3.x /= tempVector3Second.x;
-      tempVector3.y /= tempVector3Second.y;
-      tempVector3.z /= tempVector3Second.z;
+      if (tempVector3Second.x !== 0) {
+        tempVector3.x /= tempVector3Second.x;
+      } else {
+        tempVector3.x = 0;
+      }
+      if (tempVector3Second.y !== 0) {
+        tempVector3.y /= tempVector3Second.y;
+      } else {
+        tempVector3.y = 0;
+      }
+      if (tempVector3Second.z !== 0) {
+        tempVector3.z /= tempVector3Second.z;
+      } else {
+        tempVector3.z = 0;
+      }
 
       this.setPosition(tempVector3.x, tempVector3.y, tempVector3.z);
     } else {
