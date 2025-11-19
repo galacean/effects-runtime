8e572bf6acf731f9891b70af58eac226ae261020
澄弈 <chengyi.zzm@gmail.com>
2025-11-19 17:07:26 +0800

    feat: add webworker support detection

--- web-packages/demo/src/compressed.ts
+++ web-packages/demo/src/compressed.ts
@@ -1,7 +1,11 @@
 import { Player } from '@galacean/effects';
 import '@galacean/effects-plugin-ktx2';
+import '@galacean/effects-plugin-ffd';
+import '@galacean/effects-plugin-spine';
 
-const json_ktx2 = `{
+const json = 'https://mdn.alipayobjects.com/mars/afts/file/A*1zOzT6g_Mw8AAAAAQgAAAAgAelB4AQ';
+const json_ktx2_spine = 'https://mdn.alipayobjects.com/mars/afts/file/A*lnmBT7dsNNgAAAAAQFAAAAgAelB4AQ';
+const json_ktx2_ffd = `{
   "playerVersion": {
     "web": "2.7.3",
     "native": "0.0.1.202311221223"
@@ -410,7 +414,7 @@ const container = document.getElementById('J-container');
     },
   });
 
-  await player.loadScene(JSON.parse(json_ktx2), {
+  await player.loadScene(json_ktx2_ffd, {
     useCompressedTexture: true,
   });
 })();
