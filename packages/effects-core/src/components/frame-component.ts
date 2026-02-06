804ba9cbe8899d555b75ee8a0561fbd52710cfd8
wumaolinmaoan <wumaolin1998@outlook.com>
2026-02-06 11:24:25 +0800

    chore: frame component support color bagroung rendering

--- packages/effects-core/src/components/frame-component.ts
+++ packages/effects-core/src/components/frame-component.ts
@@ -1,11 +1,17 @@
 import { RendererComponent } from './renderer-component';
 import type { VFXItem } from '../vfx-item';
 import { Material, type Maskable } from '../material';
+import type { Renderer } from '../render';
 import { Geometry } from '../render';
 import { glContext } from '../gl';
 import { Matrix4 } from '@galacean/effects-math/es/core/matrix4';
+import { Color } from '@galacean/effects-math/es/core/color';
+import { effectsClass } from '../decorators';
 
+@effectsClass('FrameComponent')
 export class FrameComponent extends RendererComponent implements Maskable {
+  color = new Color(1, 1, 1, 1);
+
   private clipGeometry: Geometry;
   private worldMatrix = new Matrix4();
 
@@ -39,8 +45,11 @@ export class FrameComponent extends RendererComponent implements Maskable {
       shader: {
         fragment: `
           precision mediump float;
+
+          uniform vec4 _Color;
+
           void main() {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            gl_FragColor = _Color;
           }
         `,
         vertex: `
@@ -56,13 +65,17 @@ export class FrameComponent extends RendererComponent implements Maskable {
       },
     });
 
-    this.material = material;
     material.depthTest = true;
     material.depthMask = false;
+    material.setColor('_Color', this.color);
+
+    this.material = material;
   }
 
   override onUpdate (dt: number): void {
     this.setClipRectangle();
+
+    this.material.setColor('_Color', this.color);
   }
 
   private setClipRectangle (): void {
@@ -83,6 +96,15 @@ export class FrameComponent extends RendererComponent implements Maskable {
     }
   }
 
+  override render (renderer: Renderer): void {
+    // 直接按列缩放矩阵元素以实现右乘 size 矩阵（column-major）
+    this.worldMatrix.copyFrom(this.transform.getWorldMatrix());
+
+    multiplyMatrixByScale(this.worldMatrix, this.transform.size.x, this.transform.size.y);
+
+    renderer.drawGeometry(this.clipGeometry, this.worldMatrix, this.material);
+  }
+
   drawStencilMask (maskRef: number): void {
     if (!this.isActiveAndEnabled) {
       return;
@@ -90,17 +112,23 @@ export class FrameComponent extends RendererComponent implements Maskable {
 
     // 直接按列缩放矩阵元素以实现右乘 size 矩阵（column-major）
     this.worldMatrix.copyFrom(this.transform.getWorldMatrix());
-    const e = this.worldMatrix.elements;
-    const sx = this.transform.size.x;
-    const sy = this.transform.size.y;
 
-    // 列优先存储：elements 0-3 第一列，4-7 第二列，8-11 第三列，12-15 第四列
-    // 右乘 diag(sx, sy, 1, 1) 等价于按列分别乘以 sx, sy, 1, 1
-    e[0] *= sx; e[1] *= sx; e[2] *= sx; e[3] *= sx; // 第一列 * sx
-    e[4] *= sy; e[5] *= sy; e[6] *= sy; e[7] *= sy; // 第二列 * sy
-    // 第三列 (z) 乘 1，无需修改
-    // 第四列 (位移) 乘 1，无需修改
+    multiplyMatrixByScale(this.worldMatrix, this.transform.size.x, this.transform.size.y);
 
     this.maskManager.drawGeometryMask(this.engine.renderer, this.clipGeometry, this.worldMatrix, this.material, maskRef);
   }
+}
+
+/**
+ * 矩阵右乘 diag(sx, sy, 1, 1) 等价于按列分别乘以 sx, sy, 1, 1
+ */
+function multiplyMatrixByScale (matrix: Matrix4, sx: number, sy: number): void {
+  const e = matrix.elements;
+
+  // 列优先存储：elements 0-3 第一列，4-7 第二列，8-11 第三列，12-15 第四列
+  // 右乘 diag(sx, sy, 1, 1) 等价于按列分别乘以 sx, sy, 1, 1
+  e[0] *= sx; e[1] *= sx; e[2] *= sx; e[3] *= sx; // 第一列 * sx
+  e[4] *= sy; e[5] *= sy; e[6] *= sy; e[7] *= sy; // 第二列 * sy
+  // 第三列 (z) 乘 1，无需修改
+  // 第四列 (位移) 乘 1，无需修改
 }
