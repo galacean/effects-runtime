1e054f0d3c5ce41f9b8edee8aae7ca00748b45f5
wumaolinmaoan <wumaolin1998@outlook.com>
2026-09-23 16:13:59 +0800

    refactor: remove paas-texture-cache for improved code clarity and maintenance

--- packages/effects-core/src/index.ts
+++ packages/effects-core/src/index.ts
@@ -36,7 +36,6 @@ export * from './gl';
 export * from './gpu-resource';
 export * from './material';
 export * from './math';
-export * from './paas-texture-cache';
 export * from './plugin-system';
 export * from './scene-loader';
 export * from './plugins';
